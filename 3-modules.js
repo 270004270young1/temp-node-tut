5bdd3868e09b9b54e2e1f7a34c4165323c1cdaff
270004270young1 <youngminghei@gmail.com>
2021-06-03 13:33:45 +0800

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,12 @@
+const names = require("./4-names");
+const utils = require("./5-utils");
+const data = require("./6-alternative-flavor");
+require("./7-mind-grenade");
+/*setInterval(() => {
+    console.log(`hey it's my first node app!!!`); 
+},1000);*/
+
+
+//  console.log(names);
+// utils.sayHi('Mat');
+// utils.sayHi(names.peter);
---
+++ 8-os-module.js
@@ -0,0 +1,17 @@
+const os = require("os");
+
+const user = os.userInfo();
+
+console.log(user);
+
+
+console.log(`The system uptime is ${os.uptime} second`);
+
+const currentOS ={
+    name:os.type(),
+    release:os.release(),
+    totalMem:os.totalmem(),
+    freeMen:os.freemem(),
+}
+
+console.log(currentOS);
